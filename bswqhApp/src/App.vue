639ec85f83c0b897ac9642938acb8caae12cf5a5
DESKTOP-0URQOLM\admin <15803217937@163.com>
2020-09-28 11:06:30 +0800

    新增tab文件

--- bswqhApp/src/App.vue
+++ bswqhApp/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ bswqhApp/src/views/classification.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="classification">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
